cf7753a91046004260aed157c6d56644db618e74
Jayne Bottarini <jebottarini@gmail.com>
2020-07-28 13:22:43 -0500

    add all view controllers and segues

---
+++ COVIDiary/mapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  mapViewController.swift
+//  COVIDiary
+//
+//  Created by Jayne BOTTARINI on 28/7/2020.
+//  Copyright © 2020 Jayne BOTTARINI. All rights reserved.
+//
+
+import UIKit
+
+class mapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
