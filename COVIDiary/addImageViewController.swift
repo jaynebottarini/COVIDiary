825ae9e7b6c86ab3debf4b681c8e0b6ee2304cc0
Rajvi  Shah <rajvis2011@gmail.com>
2020-07-30 12:39:57 -0700

    with app logo

--- COVIDiary/addImageViewController.swift
+++ COVIDiary/addImageViewController.swift
@@ -96,7 +96,8 @@ class addImageViewController: UIViewController, UINavigationControllerDelegate, 
      
         /*
     // MARK: - Navigation
-
+// test-code 99
+     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
